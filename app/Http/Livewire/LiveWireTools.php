ba54e7d65bf0c00bc0a4b1a84f4502118b343c80
shubham mishra <shubham@fininfocom.com>
2023-06-30 15:44:53 +0530

    live wire tool

---
+++ app/Http/Livewire/LiveWireTools.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class LiveWireTools extends Component
+{
+    public function render()
+    {
+        return view('livewire.live-wire-tools');
+    }
+}
